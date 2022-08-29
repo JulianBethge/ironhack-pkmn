5e149eb79b531b38c0353602a4561e26f4f7efd2
julianBrandon <p0lterg3istt@gmail.com>
2022-08-30 00:23:41 +0200

    final

---
+++ javascript.js
@@ -0,0 +1,29 @@
+let count = 648;
+
+
+function render(){
+    let container = document.getElementById('pokemon');
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg" alt="" />`;
+}
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+render();
+
+prev.onclick = function(){
+    if(count > 1){
+        count = count -1;
+        render();
+    }
+
+}
+
+next.onclick = function(){
+    if (count < 649){
+        count = count +1
+        render();
+    }
+
+}
+
